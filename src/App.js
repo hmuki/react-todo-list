f0a23a8c8757b5e1c57101842e95d16faad3d68e
Muki Hoffmann <Hoffmann@FVFW9ZQGJ1WKs-MacBook-Air.local>
2020-06-01 09:47:38 +0100

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+// showing vs-code github setup
 
 function App() {
   return (
